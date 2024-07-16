979a471efa92ad65ab2d25fec1b3e7877544c874
Kiran Malieakkal <kiranmalieakkal18@gmail.com>
2024-07-17 01:56:43 +0200

    Added Controller(get by Username) and Service

--- src/main/java/com/kiran/triptrail/trip/controller/Controller.java
+++ src/main/java/com/kiran/triptrail/trip/controller/Controller.java
@@ -1,4 +1,33 @@
 package com.kiran.triptrail.trip.controller;
 
+import com.kiran.triptrail.trip.model.Trip;
+import com.kiran.triptrail.trip.model.User;
+import com.kiran.triptrail.trip.model.UserDto;
+import com.kiran.triptrail.trip.service.TripService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.net.URI;
+
+@RestController
+@RequestMapping("${api.base-path}${api.controllers.users}")
 public class Controller {
+    @Value("${api.base-path}${api.controllers.users}/")
+    public String API_CONTEXT_ROOT;
+
+    private final TripService service;
+
+    public Controller(TripService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/{userName}")
+    public ResponseEntity<UserDto> getCartById(@PathVariable String userName) {
+        User user = service.getUserByUserName(userName);
+        return ResponseEntity.ok(UserDto.fromUser(user));
+    }
+
+
 }
+
--- src/main/java/com/kiran/triptrail/trip/service/TripService.java
+++ src/main/java/com/kiran/triptrail/trip/service/TripService.java
@@ -1,4 +1,23 @@
 package com.kiran.triptrail.trip.service;
 
+import com.kiran.triptrail.country.repository.CountryRepository;
+import com.kiran.triptrail.trip.model.User;
+import com.kiran.triptrail.trip.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
 public class TripService {
+
+    private final CountryRepository countryRepository;
+    private final UserRepository userRepository;
+
+    public TripService(CountryRepository countryRepository, UserRepository userRepository) {
+        this.countryRepository = countryRepository;
+        this.userRepository = userRepository;
+    }
+
+
+    public User getUserByUserName(String userName) {
+        return userRepository.getByUserName(userName);
+    }
 }
